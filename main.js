13853d81f7f93a3add407fdd24d468c86d0ca492
tyagidevansh <devanshtyagi0607@gmail.com>
2024-08-20 23:52:36 +0530

    road

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-
-const car = new Car(100, 100, 30, 50);
 const road = new Road(canvas.width/2, canvas.width * 0.9);
-car.draw(ctx);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
--- road.js
+++ road.js
@@ -8,24 +8,38 @@ class Road{
     this.right = x+width/2;
 
     const infinity=1000000;
-    this.top = infinity;
+    this.top = -infinity;
     this.bottom = infinity;
+
+
   }
 
-  //road doesnt work
+  getLaneCenter(laneIndex) {
+    const laneWidth = this.width / this.laneCount;
+    return this.left + laneWidth/2 + laneIndex*laneWidth;
+  }
 
   draw(ctx){
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    ctx.beginPath();
-    ctx.moveTo(this.left, this.top);
-    ctx.lineTo(this.left, this.bottom);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(this.right, this.top);
-    ctx.lineTo(this.right, this.bottom);
-    ctx.stroke();
+    for (let i = 0; i <= this.laneCount; i++) {
+      const x = lerp(
+        this.left,
+        this.right,
+        i/this.laneCount
+      );
+      if (i > 0 && i < this.laneCount) {
+        ctx.setLineDash([20, 20]);
+      } else {
+        ctx.setLineDash([]);
+      }
+  
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
   }
 }
+
